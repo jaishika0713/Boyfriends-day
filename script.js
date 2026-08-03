bb4ab2fc9c8ea04104d0c7ebc0b3084bcc89e6ba
jaishika0713 <jaishika0713@gmail.com>
2026-08-03 14:49:13 +0530

    Add pagination functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Two Idiots, One Story loaded.");
+let currentPage = 0;
+
+const pages = document.querySelectorAll(".page");
+
+function nextPage() {
+
+    pages[currentPage].classList.remove("active");
+
+    currentPage++;
+
+    if(currentPage >= pages.length){
+        currentPage = pages.length - 1;
+    }
+
+    pages[currentPage].classList.add("active");
+
+}
